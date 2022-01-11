3ae701456ef931669aaffbec45a75dd7031c4eb3
helloamandaball <helloamandaball@gmail.com>
2022-01-11 15:35:53 -0500

    adjusted footer spacing

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -8,7 +8,7 @@ export const Footer = (props) => {
             <footer className="footer">
                 <p className="littleEmblem"><sup>&#x24;</sup>&#x24;</p>
                 <div className="footerDiv">
-                    <p className="footerText">&#169;2022 by </p>
+                    <p className="footerText"><span className="versionText">Version 1.0</span> &nbsp;&#169;2022 by </p>
                     <a href="https://github.com/helloamandaball"><img src={byLogo} alt="amanda" className="byLogoImg" /></a>
                 </div>
             </footer>
